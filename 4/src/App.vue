d45da7cb9bc4053ac62168e3df82b069f983b559
Yana <yana229910nug@yandex.ru>
2024-04-15 19:32:23 +0300

    4

---
+++ 4/src/App.vue
@@ -0,0 +1,34 @@
+template
+  div id=app
+    !-- img alt=Vue logo src=.assetslogo.png --
+    !-- HelloWorld msg=Welcome to Your Vue.js App  --
+    BlogComp 
+    BlogDetailsComp 
+  div
+template
+
+script
+ import HelloWorld from '.componentsHelloWorld.vue'
+import BlogComp from '.componentsBlogComp.vue'
+import BlogDetailsComp from '.componentsblogDetailsComp.vue';
+
+export default {
+  name 'App',
+  components {
+     HelloWorld,
+    BlogComp,
+    BlogDetailsComp
+  }
+}
+script
+
+style lang=scss
+#app {
+  font-family Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing antialiased;
+  -moz-osx-font-smoothing grayscale;
+  text-align center;
+  color #2c3e50;
+  margin-top 60px;
+}
+style
---
+++ 4/src/components/FotoComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <img :src="bigImg" width="800px">
+        <div class="catslist">
+            <img @click="selectImg(cat)" v-for="(cat, index) in cats" :key="index" :src="cat" alt="">
+        </div>
+        
+
+    </div>
+</template>
+
+<script>
+import img from "@/assets/img/cat3.jpg";
+    export default {
+        data() {
+            return {
+                bigImg:'',
+                cats:{
+                    cat1: require("@/assets/img/cat1.jpg"),
+                    cat2: require("@/assets/img/cat2.jpg"),
+                    cat3: img,
+                }
+            }
+        },
+        methods: {
+            selectImg(cat){
+                this.bigImg=cat;
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
