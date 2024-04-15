c303da5340deee1c4fd5fc44c9846f63a7eab05d
Yana <yana229910nug@yandex.ru>
2024-04-15 21:45:01 +0300

    5

---
+++ 5/Interno/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <HeaderComp />
+    <BlogComp />
+    <FooterComp />
+    <HeaderComp />
+    <BlogDetailsComp />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+
+import BlogComp from './components/BlogComp.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
+import BlogDetailsComp from './components/blogDetailsComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BlogComp,
+    BlogDetailsComp,
+    HeaderComp,
+    FooterComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import "scss/vars";
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthSite / 2);
+  padding-right: calc(50% - $widthSite / 2);
+}
+</style>
---
+++ 5/Interno/src/components/HelloComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 class="title">Hello!</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.title {
+    color: red;
+}
+
+</style>
